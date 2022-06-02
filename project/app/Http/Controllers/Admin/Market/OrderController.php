2a708cc2887d9344af4ddf277b98a8e85dd60359
Amir <amirQaderi381@gmail.com>
2022-06-02 18:54:54 +0430

    edit OrderController

--- project/app/Http/Controllers/Admin/Market/OrderController.php
+++ project/app/Http/Controllers/Admin/Market/OrderController.php
@@ -7,79 +7,33 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return view('admin.market.order.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function newOrders()
     {
-        //
+        return view('admin.market.order.new.orders');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function sending()
     {
-        //
+        return view('admin.market.order.sending');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function unpaid()
     {
-        //
+        return view('admin.market.order.unpaid');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function canceled()
     {
-        //
+        return view('admin.market.order.canceled');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function returned()
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return view('admin.market.order.returned');
     }
 }
