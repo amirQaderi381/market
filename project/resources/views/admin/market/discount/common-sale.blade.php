49d3dd109f9dbe805031a844b5c078dac390d930
Amir <amirQaderi381@gmail.com>
2022-06-02 17:27:51 +0430

    add common-sale page

---
+++ project/resources/views/admin/market/discount/common-sale.blade.php
@@ -0,0 +1,70 @@
+@extends('admin.layouts.master')
+
+@section('head-tag')
+    <title>تخفیف عمومی </title>
+@endsection
+
+@section('content')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item font-size-12"><a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"><a href="#"> بخش فروش</a></li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> تخفیف عمومی </li>
+        </ol>
+    </nav>
+
+    <div class="row">
+        <section class="col-12">
+            <section class="main-body-container">
+                <section class="main-body-container-header">
+                    <h5>تخفیف عمومی </h5>
+                </section>
+
+                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 pb-2 border-bottom">
+                    <a href="{{ route('admin.market.discount.common.sale.create') }}" class="btn btn-info btn-sm"> ایجاد
+                        تخفیف عمومی
+                        جدید</a>
+                    <div>
+                        <input type="text" class="form-control form-control-sm max-width-16-rem" placeholder="جستجو">
+                    </div>
+                </section>
+
+                <section class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">درصد تخفیف </th>
+                                <th scope="col">سقف تخفیف </th>
+                                <th scope="col">عنوان مناسبت </th>
+                                <th scope="col">تاریخ شروع</th>
+                                <th scope="col">تاریخ پایان</th>
+                                <th scope="col" class="max-width-16-rem text-center"><i class="fas fa-cogs"></i> تنظیمات
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td>15% </td>
+                                <td>26000 تومان </td>
+                                <td> میلاد با سعادت امام علی </td>
+                                <td> 24 اردیبهشت 1401 </td>
+                                <td> 31 اردیبهشت 1401 </td>
+                                <td class="width-16-rem text-left">
+                                    <button type="submit" class="btn btn-primary btn-sm">
+                                        <i class="fas fa-edit"></i> ویرایش
+                                    </button>
+
+                                    <button type="submit" class="btn btn-danger btn-sm">
+                                        <i class="fas fa-trash-alt"></i> حذف
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </section>
+        </section>
+    </div>
+@endsection
