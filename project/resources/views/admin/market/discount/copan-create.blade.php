fffd82bfc91e483b2bfd9693de2b138528b27794
Amir <amirQaderi381@gmail.com>
2022-06-02 16:47:44 +0430

    edit copan-create view

--- project/resources/views/admin/market/discount/copan-create.blade.php
+++ project/resources/views/admin/market/discount/copan-create.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-    <title>ایجاد روش ارسال</title>
+    <title>کوپن تخفیف</title>
 @endsection
 
 @section('content')
@@ -9,8 +9,8 @@
         <ol class="breadcrumb">
             <li class="breadcrumb-item font-size-12"><a href="#">خانه</a></li>
             <li class="breadcrumb-item font-size-12"><a href="#"> بخش فروش</a></li>
-            <li class="breadcrumb-item font-size-12"><a href="#"> روش های ارسال</a> </li>
-            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد روش ارسال</li>
+            <li class="breadcrumb-item font-size-12"><a href="#"> کوپن تخفیف</a> </li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد کوپن تخفیف</li>
         </ol>
     </nav>
 
@@ -18,26 +18,45 @@
         <section class="col-12">
             <section class="main-body-container">
                 <section class="main-body-container-header">
-                    <h5>ایجاد روش ارسال</h5>
+                    <h5>ایجاد کوپن تخفیف</h5>
                 </section>
 
                 <section class="mt-4 mb-3 pb-2 border-bottom">
-                    <a href="{{ route('admin.market.delivery.index') }}" class="btn btn-info btn-sm">بازگشت</a>
+                    <a href="{{ route('admin.market.discount.copan') }}" class="btn btn-info btn-sm">بازگشت</a>
                 </section>
 
                 <section>
                     <form action="#">
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="">نام روش ارسال</label>
+                                <label for="">کد کوپن </label>
                                 <input type="text" class="form-control form-control-sm">
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="">هزینه ارسال</label>
+                                <label for="">نوع کوپن</label>
+                                <select id="" class="form-control form-control-sm">
+                                    <option selected>عمومی</option>
+                                    <option>خصوصی</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="">درصد تخفیف</label>
                                 <input type="text" class="form-control form-control-sm">
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="">زمان ارسال</label>
+                                <label for="">حداکثر تخفیف</label>
+                                <input type="text" class="form-control form-control-sm">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="">عنوان مناسبت</label>
+                                <input type="text" class="form-control form-control-sm">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="">تاریخ شروع</label>
+                                <input type="text" class="form-control form-control-sm">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="">تاریخ پایان</label>
                                 <input type="text" class="form-control form-control-sm">
                             </div>
                         </div>
