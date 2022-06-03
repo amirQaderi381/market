6453dbbfdbdd4aca58727d90effbe7e60757d4f5
Amir <amirQaderi381@gmail.com>
2022-06-03 17:30:58 +0430

    edit paymentController

--- project/app/Http/Controllers/Admin/Market/PaymentController.php
+++ project/app/Http/Controllers/Admin/Market/PaymentController.php
@@ -7,5 +7,28 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.market.payment.index');
+    }
+
+    public function online()
+    {
+        return view('admin.market.payment.index');
+    }
+
+    public function offline()
+    {
+        return view('admin.market.payment.index');
+    }
+
+    public function attendance()
+    {
+        return view('admin.market.payment.index');
+    }
+
+    public function confirm()
+    {
+        return view('admin.market.payment.index');
+    }
 }
