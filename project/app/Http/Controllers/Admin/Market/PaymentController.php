f140e1879abd83502e5cedf8a5c8211a0c1cd70e
Amir <amirQaderi381@gmail.com>
2022-06-03 17:29:24 +0430

    add payment controller

---
+++ project/app/Http/Controllers/Admin/Market/PaymentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Market;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    //
+}
