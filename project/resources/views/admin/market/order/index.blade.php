45f9fab497edfd8940d182abde9c86dcf398885c
Amir <amirQaderi381@gmail.com>
2022-06-03 12:04:12 +0430

    edit index page from order folder

--- project/resources/views/admin/market/order/index.blade.php
+++ project/resources/views/admin/market/order/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-    <title>کوپن تخفیف </title>
+    <title>سفارشات</title>
 @endsection
 
 @section('content')
@@ -9,7 +9,7 @@
         <ol class="breadcrumb">
             <li class="breadcrumb-item font-size-12"><a href="#">خانه</a></li>
             <li class="breadcrumb-item font-size-12"><a href="#"> بخش فروش</a></li>
-            <li class="breadcrumb-item font-size-12 active" aria-current="page"> کوپن تخفیف</li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> سفارشات</li>
         </ol>
     </nav>
 
@@ -17,12 +17,11 @@
         <section class="col-12">
             <section class="main-body-container">
                 <section class="main-body-container-header">
-                    <h5>کوپن تخفیف </h5>
+                    <h5>سفارشات </h5>
                 </section>
 
                 <section class="d-flex justify-content-between align-items-center mt-4 mb-3 pb-2 border-bottom">
-                    <a href="{{ route('admin.market.discount.copan.create') }}" class="btn btn-info btn-sm"> ایجاد کوپن
-                        تخفیف
+                    <a href="{{ route('admin.market.order.new.orders') }}" class="btn btn-info btn-sm"> ایجاد سفارش
                         جدید</a>
                     <div>
                         <input type="text" class="form-control form-control-sm max-width-16-rem" placeholder="جستجو">
@@ -34,12 +33,16 @@
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
-                                <th scope="col">کد کوپن </th>
-                                <th scope="col">درصد تخفیف </th>
-                                <th scope="col">سقف تخفیف </th>
-                                <th scope="col">نوع کوپن </th>
-                                <th scope="col">تاریخ شروع</th>
-                                <th scope="col">تاریخ پایان</th>
+                                <th scope="col">کد سفارش</th>
+                                <th scope="col">مبلغ سفارش</th>
+                                <th scope="col">مبلغ تخفیف</th>
+                                <th scope="col">مبلغ نهایی</th>
+                                <th scope="col">وضعیت پرداخت</th>
+                                <th scope="col">شیوه پرداخت</th>
+                                <th scope="col">بانک</th>
+                                <th scope="col">وضعیت ارسال</th>
+                                <th scope="col">شیوه ارسال</th>
+                                <th scope="col">وضعیت سفارش</th>
                                 <th scope="col" class="max-width-16-rem text-center"><i class="fas fa-cogs"></i> تنظیمات
                                 </th>
                             </tr>
@@ -47,21 +50,40 @@
                         <tbody>
                             <tr>
                                 <th>1</th>
-                                <td>ass25fg </td>
-                                <td>15% </td>
-                                <td>26000 تومان </td>
-                                <td> عمومی </td>
-                                <td> 24 اردیبهشت 1401 </td>
-                                <td> 31 اردیبهشت 1401 </td>
-                                <td class="width-16-rem text-left">
-                                    <button type="submit" class="btn btn-primary btn-sm">
-                                        <i class="fas fa-edit"></i> ویرایش
-                                    </button>
-
-                                    <button type="submit" class="btn btn-danger btn-sm">
-                                        <i class="fas fa-trash-alt"></i> حذف
+                                <td>123456</td>
+                                <td>26000 تومان</td>
+                                <td>20000 تومان</td>
+                                <td>26000 تومان</td>
+                                <td><i class="fas fa-credit-card"></i> پرداخت شده</td>
+                                <td>آنلاین</td>
+                                <td>ملت</td>
+                                <td><i class="fas fa-clock"></i> درحال ارسال</td>
+                                <td>پیک موتوری</td>
+                                <td>درحال ارسال</td>
+                                <td class="text-left">
+                                    <button type="submit" class="btn btn-success btn-sm">
+                                        <i class="fas fa-cogs"></i> عملیات
                                     </button>
                                 </td>
+                                <section class="order-operation rounded">
+                                    <section class="list-group rounded">
+                                        <a href="#" class="list-group-item list-group-item-action header-profile-link">
+                                            <i class="fas fa-cog"></i> تنظیمات
+                                        </a>
+                                        <a href="#" class="list-group-item list-group-item-action header-profile-link">
+                                            <i class="fas fa-user"></i> کاربر
+                                        </a>
+                                        <a href="#" class="list-group-item list-group-item-action header-profile-link">
+                                            <i class="far fa-envelope"></i> پیام ها
+                                        </a>
+                                        <a href="#" class="list-group-item list-group-item-action header-profile-link">
+                                            <i class="fas fa-lock"></i> قفل صفحه
+                                        </a>
+                                        <a href="#" class="list-group-item list-group-item-action header-profile-link">
+                                            <i class="fas fa-sign-out-alt"></i> خروج
+                                        </a>
+                                    </section>
+                                </section>
                             </tr>
                         </tbody>
                     </table>
