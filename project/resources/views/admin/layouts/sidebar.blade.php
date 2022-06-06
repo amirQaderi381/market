888a652bace7de14b9a7eda43474756e864c19b1
Amir <amirQaderi381@gmail.com>
2022-06-06 20:22:24 +0430

    edit sidebar

--- project/resources/views/admin/layouts/sidebar.blade.php
+++ project/resources/views/admin/layouts/sidebar.blade.php
@@ -76,7 +76,7 @@
 
 
             <section class="sidebar-part-title">بخش محتوی</section>
-            <a href="#" class="sidebar-link">
+            <a href="{{ route('admin.content.category.index') }}" class="sidebar-link">
                 <i class="fas fa-bars"></i>
                 <span>دسته بندی</span>
             </a>
