1a611b7638f5ee91c48bf0763659dfddba776222
Amir <amirQaderi381@gmail.com>
2022-06-06 20:22:13 +0430

    edit content category controller

--- project/app/Http/Controllers/Admin/Content/CategoryController.php
+++ project/app/Http/Controllers/Admin/Content/CategoryController.php
@@ -24,7 +24,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        return view('admin.content.category.index');
+        return view('admin.content.category.create');
     }
 
     /**
