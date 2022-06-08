1c98de9b2c7b2c0e5ef52d08350fa84d6c43890c
Amir <amirQaderi381@gmail.com>
2022-06-08 14:12:12 +0430

    add ticket routes

--- project/routes/web.php
+++ project/routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Admin\Market\OrderController;
 use App\Http\Controllers\Admin\Market\StoreController;
 use App\Http\Controllers\Admin\Notify\EmailController;
 use App\Http\Controllers\Admin\AdminDashbordController;
+use App\Http\Controllers\Admin\Ticket\TicketController;
 use App\Http\Controllers\Admin\User\CustomerController;
 use App\Http\Controllers\Admin\Market\CommentController;
 use App\Http\Controllers\Admin\Market\GalleryController;
@@ -332,5 +333,21 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
             Route::put('/update/{id}', [SMSController::class, 'update'])->name('admin.notify.sms.update');
             Route::delete('/destroy/{id}', [SMSController::class, 'destroy'])->name('admin.notify.sms.destroy');
         });
+    });
+
+    //ticket
+
+    Route::prefix('ticket')->namespace('Ticket')->group(function () {
+
+        Route::get('/new-tickets', [TicketController::class, 'newTickets'])->name('admin.ticket.newTickets');
+        Route::get('/open-tickets', [TicketController::class, 'openTickets'])->name('admin.ticket.openTickets');
+        Route::get('/close-tickets', [TicketController::class, 'closeTickets'])->name('admin.ticket.closeTickets');
+        Route::get('/', [TicketController::class, 'index'])->name('admin.ticket.index');
+        Route::get('/create', [TicketController::class, 'create'])->name('admin.ticket.create');
+        Route::get('/show', [TicketController::class, 'show'])->name('admin.ticket.show');
+        Route::post('/store', [TicketController::class, 'store'])->name('admin.ticket.store');
+        Route::get('/edit/{id}', [TicketController::class, 'edit'])->name('admin.ticket.edit');
+        Route::put('/update/{id}', [TicketController::class, 'update'])->name('admin.ticket.update');
+        Route::delete('/destroy/{id}', [TicketController::class, 'destroy'])->name('admin.ticket.destroy');
     });
 });
