5c3fc321900d1e4c4532eca1091ab423cbc0b68d
Amir <amirQaderi381@gmail.com>
2022-06-17 16:21:12 +0430

    edit menu migration

--- project/database/migrations/2022_06_10_132325_create_menus_table.php
+++ project/database/migrations/2022_06_10_132325_create_menus_table.php
@@ -18,7 +18,7 @@ class CreateMenusTable extends Migration
             $table->string('name');
             $table->string('url');
             $table->tinyInteger('status')->default(0);
-            $table->foreignId('parent_id')->constrained('menus');
+            $table->foreignId('parent_id')->nullable()->constrained('menus');
             $table->timestamps();
             $table->softDeletes();
         });
