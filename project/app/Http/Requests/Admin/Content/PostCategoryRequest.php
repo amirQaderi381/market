858cf70d964def5516f2f284bf9dd6031384e1a4
amir <amirqaderi381@gmail.com>
2022-07-15 15:55:31 +0430

    add postCategoryRequest

---
+++ project/app/Http/Requests/Admin/Content/PostCategoryRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests\Admin\Content;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        if($this->isMethod('post'))
+        {
+            return [
+
+                'name'=>'required|min:2|max:120',
+                'discription'=>'required|min:5|max:500',
+                'slug'=>'nullable',
+                'image'=>'required',
+                'status'=>'required|numeric|in:0,1',
+                'tags'=>'required'
+            ];
+
+        }else
+        {
+            return [
+
+                'name'=>'required|min:2|max:120',
+                'discription'=>'required|min:5|max:500',
+                'slug'=>'nullable',
+                'image'=>'',
+                'status'=>'required|numeric|in:0,1',
+                'tags'=>'required'
+            ];
+
+        }
+
+    }
+}
