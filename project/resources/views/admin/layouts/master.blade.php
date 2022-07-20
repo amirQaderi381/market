be31e4399c9f093f4031d6be76908db7441c35c0
amir <amirqaderi381@gmail.com>
2022-07-20 15:21:37 +0430

    edit toast alert

--- project/resources/views/admin/layouts/master.blade.php
+++ project/resources/views/admin/layouts/master.blade.php
@@ -25,6 +25,13 @@
     @include('admin.layouts.scripts')
     @yield('script')
 
+    <section class="toast-wrapper flex-row-reverse">
+         @include('admin.alerts.toast.success')
+         @include('admin.alerts.toast.error')
+    </section>
+
+    @include('admin.alerts.sweetalert.success')
+    @include('admin.alerts.sweetalert.error')
 </body>
 
 </html>
