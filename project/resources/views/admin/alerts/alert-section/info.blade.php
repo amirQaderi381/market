e7a401e8915ea6653d7bd5bb262a5dc675c5007e
amir <amirqaderi381@gmail.com>
2022-07-20 13:44:23 +0430

    add alert-section folder

---
+++ project/resources/views/admin/alerts/alert-section/info.blade.php
@@ -0,0 +1,12 @@
+@if (session('alert-section-info'))
+    <section class="alert alert-info alert-dismissible fade show" role="alert">
+        <h4 class="alert-heading"> &times; اطلاعیه </h4>
+        <hr>
+        <p class="mb-0">
+            {{ session('alert-section-info') }}
+        </p>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="right:auto !important ; left:0 !important">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </section>
+@endif
