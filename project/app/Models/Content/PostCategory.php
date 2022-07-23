8e66643622ca1502feb86641a47cfd4385f6b47e
amir <amirqaderi381@gmail.com>
2022-07-23 16:08:28 +0430

    add slug package

--- project/app/Models/Content/PostCategory.php
+++ project/app/Models/Content/PostCategory.php
@@ -5,10 +5,22 @@ namespace App\Models\Content;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class PostCategory extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory,SoftDeletes,Sluggable;
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 
     protected $fillable=['name','description','slug','image','status','tags'];
+
+
 }
