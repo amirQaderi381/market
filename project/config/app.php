2be8b685d33db9f8c5a0710f8a8445c342b9d99b
amir <amirqaderi381@gmail.com>
2022-07-26 15:48:20 +0430

    edit config/app.php

--- project/config/app.php
+++ project/config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        // Intervention\Image\ImageServiceProvider::class
 
     ],
 
