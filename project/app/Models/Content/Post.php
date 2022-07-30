347384c40ef94659651b5f853eae60277863334a
amir <amirqaderi381@gmail.com>
2022-07-30 17:42:58 +0430

    add post model

---
+++ project/app/Models/Content/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\Content;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory,SoftDeletes,sluggable;
+
+    public function sluggable(): array {
+
+        return [
+            'slug' => 'title'
+        ];
+    }
+
+    protected $fillable=['title','slug','summary','body','image','status','commentable','tags','published_at','author_id','category_id'];
+
+    protected $casts=['image' => 'array'];
+
+    public function postCategory()
+    {
+       return  $this->belongsTo(PostCategory::class,'category_id');
+    }
+
+}
+
