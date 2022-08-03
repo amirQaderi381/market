0b409b50bd5f81a8c3a73327bc7243ec6bd57d31
amir <amirqaderi381@gmail.com>
2022-08-03 15:46:41 +0430

    edit faq request

--- project/app/Http/Requests/Admin/Content/FaqRequest.php
+++ project/app/Http/Requests/Admin/Content/FaqRequest.php
@@ -23,26 +23,12 @@ class FaqRequest extends FormRequest
      */
     public function rules()
     {
-        if($this->isMethod('post'))
-        {
             return [
 
-                'question'=>'required|min:5|max:300|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.?, ]+$/u',
+                'question'=>'required|min:5|max:300|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.?؟, ]+$/u',
                 'answer'=>'required|min:2|max:500|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.,><\/;\n\r& ]+$/u',
                 'status'=>'required|numeric|in:0,1',
                 'tags'=>'required|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.?, ]+$/u',
             ];
-
-        }else{
-
-            return [
-
-                'question'=>'required|min:5|max:300|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.?, ]+$/u',
-                'answer'=>'required|min:2|max:500|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.,><\/;\n\r& ]+$/u',
-                'status'=>'required|numeric|in:0,1',
-                'tags'=>'required|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.?, ]+$/u',
-            ];
-
-        }
     }
 }
