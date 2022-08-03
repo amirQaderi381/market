450056324bae96773b6917facddeb5cabfd59410
amir <amirqaderi381@gmail.com>
2022-08-03 14:45:56 +0430

    edit admin/content/faq/create

--- project/resources/views/admin/content/faq/create.blade.php
+++ project/resources/views/admin/content/faq/create.blade.php
@@ -26,15 +26,63 @@
                 </section>
 
                 <section>
-                    <form action="#">
+                    <form action="{{ route('admin.content.faq.store') }}" method="POST" id="form">
+                        @csrf
                         <div class="form-row">
-                            <div class="form-group col-12">
-                                <label for="">پرسش</label>
-                                <input type="text" class="form-control form-control-sm">
+                            <div class="col-12 my-2">
+                                <div class="form-group">
+                                    <label for="question">پرسش</label>
+                                    <input type="text" class="form-control form-control-sm " name="question"
+                                        value="{{ old('question') }}">
+                                </div>
+                                @error('question')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
                             </div>
-                            <div class="form-group col-12">
-                                <label for="">پاسخ</label>
-                                <textarea class="form-control form-control-sm" name="body" id="body" rows="4"></textarea>
+                            <div class="col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="status">وضعیت</label>
+                                    <select id="status" class="form-control form-control-sm" name="status">
+                                        <option value="0" {{ old('status') == 0 ? 'selected' : '' }}>غیر فعال</option>
+                                        <option value="1" {{ old('status') == 1 ? 'selected' : '' }}>فعال</option>
+                                    </select>
+                                </div>
+                                @error('status')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="tags">برچسب ها</label>
+                                    <input type="hidden" class="form-control form-control-sm" name="tags" id="tags"
+                                        value="{{ old('tags') }}">
+                                    <select class="select2 form-control form-control-sm" id="select_tags" multiple></select>
+                                </div>
+                                @error('tags')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="col-12 my-2">
+                                <div class="form-group">
+                                    <label for="answer">پاسخ</label>
+                                    <textarea class="form-control form-control-sm" name="answer" id="body" rows="4">{{ old('answer') }}</textarea>
+                                </div>
+                                @error('answer')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary btn-sm px-5">ثبت</button>
@@ -49,5 +97,31 @@
     <script src="{{ asset('admin_assets/ckeditor/ckeditor.js') }}"></script>
     <script>
         CKEDITOR.replace('body');
+    </script>
+    <script>
+        let tags_input = $('#tags');
+        let select_tags = $('#select_tags');
+        let default_tags = tags_input.val();
+        let default_data = null;
+
+        if (default_tags !== null && default_tags.length > 0) {
+            default_data = default_tags.split(',');
+        }
+
+        select_tags.select2({
+            placeholder: 'لطفا برچسب ها را وارد کنید',
+            tags: true,
+            data: default_data
+        });
+
+        select_tags.children('option').attr('selected', true).trigger('change');
+
+        $('#form').submit(function() {
+
+            if (select_tags.val() !== null && select_tags.val().length > 0) {
+                let selected_source = select_tags.val().join(',');
+                tags_input.val(selected_source);
+            }
+        })
     </script>
 @endsection
