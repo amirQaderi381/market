e90a50d65cb5406229fb212827dff93489da1025
amir <amirqaderi381@gmail.com>
2022-08-05 13:56:53 +0430

    complete menu views

--- project/resources/views/admin/content/menu/edit.blade.php
+++ project/resources/views/admin/content/menu/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-    <title>ایجاد منو</title>
+    <title>ویرایش منو</title>
 @endsection
 
 @section('content')
@@ -10,7 +10,7 @@
             <li class="breadcrumb-item font-size-12"><a href="#">خانه</a></li>
             <li class="breadcrumb-item font-size-12"><a href="#"> بخش محتوی</a></li>
             <li class="breadcrumb-item font-size-12"><a href="#"> منو</a> </li>
-            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد منو</li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ویرایش منو</li>
         </ol>
     </nav>
 
@@ -18,7 +18,7 @@
         <section class="col-12">
             <section class="main-body-container">
                 <section class="main-body-container-header">
-                    <h5>ایجاد منو</h5>
+                    <h5>ویرایش منو</h5>
                 </section>
 
                 <section class="mt-4 mb-3 pb-2 border-bottom">
@@ -26,14 +26,15 @@
                 </section>
 
                 <section>
-                    <form action="{{ route('admin.content.menu.store') }}" method="post">
+                    <form action="{{ route('admin.content.menu.update',$menu->id) }}" method="post">
                         @csrf
+                        {{ method_field('put') }}
                         <div class="form-row">
                             <div class="col-md-6">
-                                <div class="form-group">
+                                <div class="form-group my-2">
                                     <label for="name">عنوان منو</label>
                                     <input type="text" class="form-control form-control-sm" name="name"
-                                        value="{{ old('name') }}">
+                                        value="{{ old('name',$menu->name) }}">
                                 </div>
                                 @error('name')
                                     <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
@@ -42,13 +43,13 @@
                                 @enderror
                             </div>
                             <div class="col-md-6">
-                                <div class="form-group">
+                                <div class="form-group my-2">
                                     <label for="parent_id">منو والد</label>
                                     <select id="parent_id" name="parent_id" class="form-control form-control-sm">
                                         <option value="">منو اصلی</option>
                                         @foreach ($parent_menus as $parent_menu)
                                             <option value="{{ $parent_menu->id }}"
-                                                @if (old('parent_id') == $parent_menu->id) selected @endif>{{ $parent_menu->name }}
+                                                @if (old('parent_id',$menu->parent_id) == $parent_menu->id) selected @endif>{{ $parent_menu->name }}
                                             </option>
                                         @endforeach
                                     </select>
@@ -60,10 +61,9 @@
                                 @enderror
                             </div>
                             <div class="col-md-6">
-                                <div class="form-group">
+                                <div class="form-group my-2">
                                     <label for="url">آدرس url</label>
-                                    <input type="text" class="form-control form-control-sm" name="url"
-                                        value="{{ old('url') }}">
+                                    <input type="text" class="form-control form-control-sm" name="url" value="{{ old('url',$menu->url) }}">
                                 </div>
                                 @error('url')
                                     <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
@@ -71,17 +71,17 @@
                                     </span>
                                 @enderror
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="status">وضعیت</label>
                                     <select id="status" class="form-control form-control-sm" name="status">
-                                        <option value="0" {{ old('status') == 0 ? 'selected' : '' }}>غیر فعال</option>
-                                        <option value="1" {{ old('status') == 1 ? 'selected' : '' }}>فعال</option>
+                                        <option value="0" {{ old('status',$menu->status) == 0 ? 'selected' : '' }}>غیر فعال</option>
+                                        <option value="1" {{ old('status',$menu->status) == 1 ? 'selected' : '' }}>فعال</option>
                                     </select>
                                 </div>
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-primary btn-sm px-5">ثبت</button>
+                        <button type="submit" class="btn btn-primary btn-sm px-5 my-2">ثبت</button>
                     </form>
                 </section>
             </section>
