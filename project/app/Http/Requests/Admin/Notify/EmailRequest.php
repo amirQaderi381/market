e87d0305b1b7f006285b273bf7637ec1064cce9c
amir <amirqaderi381@gmail.com>
2022-08-09 20:08:52 +0430

    add email request

---
+++ project/app/Http/Requests/Admin/Notify/EmailRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Admin\Notify;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+
+            'subject'=>'required|min:2|max:120|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+            'body'=>'required|min:5|max:500|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي.,><\/;\n\r& ]+$/u',
+            'status'=>'required|numeric|in:0,1',
+            'published_at' => 'required|numeric'
+        ];
+    }
+}
