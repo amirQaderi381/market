c8dd728baf748727995041ce9aa7c10ecd0aa425
amir <amirqaderi381@gmail.com>
2022-08-11 14:00:08 +0430

    add email file controller

---
+++ project/app/Http/Controllers/Admin/Notify/EmailFileController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Notify;
+
+use App\Models\Notify\Email;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Notify\EmailFile;
+
+class EmailFileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Email $email)
+    {
+        return view('admin.notify.email_file.index',compact('email'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function status(EmailFile $emailFile)
+    {
+        $emailFile->status = $emailFile->status == 0 ?  1 : 0;
+        $result = $emailFile->save();
+
+        if($result)
+        {
+            if($emailFile->status == 0)
+            {
+               return response()->json(['status' => true , 'checked' => false]);
+
+            }else{
+
+                return response()->json(['status' => true , 'checked' => true]);
+            }
+
+        }else{
+
+            return response()->json(['status' => false]);
+        }
+    }
+}
