5f5c3652b2009eb2d25d3333e801a06725d19321
amir <amirqaderi381@gmail.com>
2022-08-11 15:40:41 +0430

    edit email index

--- project/resources/views/admin/notify/email/index.blade.php
+++ project/resources/views/admin/notify/email/index.blade.php
@@ -58,7 +58,7 @@
                                     </td>
                                     <td class="max-width-16-rem text-left">
                                         <a href="{{ route('admin.notify.email-file.index',$email->id) }}" class="btn btn-warning btn-sm">
-                                            <i class="fa fa-edit"></i> فایل های ضمیمه شده
+                                            <i class="fa fa-file"></i> فایل های ضمیمه شده
                                         </a>
                                         <a href="{{ route('admin.notify.email.edit',$email->id) }}" class="btn btn-info btn-sm">
                                             <i class="fa fa-edit"></i> ویرایش
