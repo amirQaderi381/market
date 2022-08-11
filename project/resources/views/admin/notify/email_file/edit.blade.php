f1ea24fd2c23e04510e3c26e2c587a758be1e9ec
amir <amirqaderi381@gmail.com>
2022-08-11 14:01:04 +0430

    add email file views

---
+++ project/resources/views/admin/notify/email_file/edit.blade.php
@@ -0,0 +1,131 @@
+@extends('admin.layouts.master')
+
+@section('head-tag')
+    <title>ویرایش اطلاعیه ایمیلی</title>
+    <link rel="stylesheet" href="{{ asset('admin_assets/datepicker/persian-datepicker.min.css') }}">
+@endsection
+
+@section('content')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">اطلاع رسانی</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">اطلاعیه ایمیلی</a></li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ویرایش اطلاعیه ایمیلی</li>
+        </ol>
+    </nav>
+
+
+    <section class="row">
+        <section class="col-12">
+            <section class="main-body-container">
+                <section class="main-body-container-header">
+                    <h5>
+                        ویرایش اطلاعیه ایمیلی
+                    </h5>
+                </section>
+
+                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                    <a href="{{ route('admin.notify.email.index') }}" class="btn btn-info btn-sm">بازگشت</a>
+                </section>
+
+                <section>
+                    <form action="{{ route('admin.notify.email.update',$email->id) }}" method="post">
+                        @csrf
+                        {{ method_field('put') }}
+                        <section class="row">
+                            <section class="col-12 col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="subject">عنوان ایمیل</label>
+                                    <input type="text" name="subject" class="form-control form-control-sm"
+                                        value="{{ old('subject',$email->subject) }}">
+                                </div>
+                                @error('subject')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+                            <section class="col-12 col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="published_at"> تاریخ انتشار</label>
+                                    <input type="hidden" name="published_at" class="form-control form-control-sm"
+                                        id="published_at">
+                                    <input type="tedxt" class="form-control form-control-sm" id="published_at_view" value="{{ old('published_at',$email->published_at) }}">
+                                </div>
+                                @error('published_at')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+                            <section class="col-12 my-2">
+                                <section class="form-group">
+                                    <label for="status">وضعیت</label>
+                                    <select class="form-control form-control-sm" name="status" id="status">
+                                        <option value="0" @if (old('status',$email->status) == 0) selected @endif>غیر فعال
+                                        </option>
+                                        <option value="1" @if (old('status',$email->status) == 1) selected @endif>فعال
+                                        </option>
+                                    </select>
+                                </section>
+                                @error('status')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+                            <section class="col-12 my-2">
+                                <div class="form-group">
+                                    <label for="body">متن ایمیل</label>
+                                    <textarea name="body" id="body" class="form-control form-control-sm" rows="6">{{ old('body',$email->body) }}</textarea>
+                                </div>
+                                @error('body')
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+                            </section>
+
+                            <section class="col-12">
+                                <button class="btn btn-primary btn-sm">ثبت</button>
+                            </section>
+                        </section>
+                    </form>
+                </section>
+
+            </section>
+        </section>
+    </section>
+@endsection
+@section('script')
+    <script src="{{ asset('admin_assets/ckeditor/ckeditor.js') }}"></script>
+    <script src="{{ asset('admin_assets/datepicker/persian-date.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/datepicker/persian-datepicker.min.js') }}"></script>
+    <script>
+        $('#published_at_view').persianDatepicker({
+            format: 'LLLL',
+            altField: '#published_at',
+            timePicker: {
+                enabled: true,
+                meridiem: {
+                    enabled: true
+                }
+            }
+        });
+    </script>
+    <script>
+        CKEDITOR.replace('body');
+    </script>
+@endsection
