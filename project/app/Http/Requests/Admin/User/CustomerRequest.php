4872d56590faa3e037cd011e1fc59fe313a8bda6
amir <amirqaderi381@gmail.com>
2022-08-13 21:46:52 +0430

    add customer request

---
+++ project/app/Http/Requests/Admin/User/CustomerRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests\Admin\User;
+
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if($this->isMethod('post')){
+
+            return [
+
+                'first_name'=>'required|min:2|max:120|regex:/^[ا-یa-zA-Zء-ي ]+$/u',
+                'last_name'=>'required|min:2|max:120|regex:/^[ا-یa-zA-Zء-ي ]+$/u',
+                'mobile'=>'required|digits:11|unique:users',
+                'national_code'=>'required|digits:10|unique:users',
+                'email'=>'required|string|email|unique:users',
+                'password' => ['required','unique:users',Password::min(8)->letters()->mixedCase()->numbers()->symbols()->uncompromised(),'confirmed'],
+                'profile_photo_path'=>'nullable|image|mimes:png,jpg,jpeg,gif',
+                'status'=>'required|numeric|in:0,1',
+                'activation'=>'required|numeric|in:0,1',
+            ];
+        }else
+        {
+            return [
+                'first_name'=>'required|min:2|max:120|regex:/^[ا-یa-zA-Zء-ي ]+$/u',
+                'last_name'=>'required|min:2|max:120|regex:/^[ا-یa-zA-Zء-ي ]+$/u',
+                'profile_photo_path'=>'nullable|image|mimes:png,jpg,jpeg,gif',
+                'status'=>'required|numeric|in:0,1',
+                'activation'=>'required|numeric|in:0,1',
+            ];
+        }
+
+    }
+}
