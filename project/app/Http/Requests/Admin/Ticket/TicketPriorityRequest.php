03caa8accc37542a0b729dfaa9b2f725f07890da
amir <amirqaderi381@gmail.com>
2022-08-14 14:38:09 +0430

    add ticket priority request

---
+++ project/app/Http/Requests/Admin/Ticket/TicketPriorityRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin\Ticket;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TicketPriorityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+
+            'name'=>'required|min:2|max:120|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+            'status'=>'required|numeric|in:0,1',
+        ];
+    }
+}
