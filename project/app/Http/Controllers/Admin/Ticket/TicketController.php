94e521f4ac1f04cf64e971530318cf088b9e5589
amir <amirqaderi381@gmail.com>
2022-08-15 15:30:39 +0430

    edit ticket controller

--- project/app/Http/Controllers/Admin/Ticket/TicketController.php
+++ project/app/Http/Controllers/Admin/Ticket/TicketController.php
@@ -2,25 +2,35 @@
 
 namespace App\Http\Controllers\Admin\Ticket;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Ticket\Ticket;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Ticket\TicketRequest;
 
 class TicketController extends Controller
 {
 
     public function newTickets()
     {
-        return view('admin.ticket.index');
+        $tickets = Ticket::where('seen',0)->get();
+        foreach($tickets as $newTicket)
+        {
+            $newTicket->seen = 1;
+            $result = $newTicket->save();
+        }
+        return view('admin.ticket.index',compact('tickets'));
     }
 
     public function openTickets()
     {
-        return view('admin.ticket.index');
+        $tickets = Ticket::where('status',0)->get();
+        return view('admin.ticket.index',compact('tickets'));
     }
 
     public function closeTickets()
     {
-        return view('admin.ticket.index');
+        $tickets = Ticket::where('status',1)->get();
+        return view('admin.ticket.index',compact('tickets'));
     }
 
     /**
@@ -30,7 +40,8 @@ class TicketController extends Controller
      */
     public function index()
     {
-        return view('admin.ticket.index');
+        $tickets = Ticket::all();
+        return view('admin.ticket.index',compact('tickets'));
     }
 
     /**
@@ -60,9 +71,9 @@ class TicketController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show(Ticket $ticket)
     {
-        return view('admin.ticket.show');
+        return view('admin.ticket.show',compact('ticket'));
     }
 
     /**
@@ -97,5 +108,30 @@ class TicketController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function change(Ticket $ticket)
+    {
+        $ticket->status = $ticket->status == 0 ? 1 : 0;
+        $result = $ticket->save();
+        if($result)
+        {
+          return redirect()->route('admin.ticket.index')->with('swal-success','تغییر شما با موفقیت ثبت شد');
+        }
+    }
+
+    public function answer(TicketRequest $request ,Ticket $ticket)
+    {
+       $inputs = $request->all();
+       $inputs['subject'] = $ticket->subject;
+       $inputs['description'] = $request->description;
+       $inputs['reference_id'] = $ticket->reference_id;
+       $inputs['user_id'] = 1;
+       $inputs['seen'] = 1;
+       $inputs['category_id'] = $ticket->category_id;
+       $inputs['priority_id'] = $ticket->priority_id;
+       $inputs['ticket_id'] = $ticket->id;
+       Ticket::create($inputs);
+       return redirect()->route('admin.ticket.index')->with('swal-success','پاسخ شما با موفقیت ثبت شد');
     }
 }
