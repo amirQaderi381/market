73e535524abf77569427d9d8f556549f808fb65b
amir <amirqaderi381@gmail.com>
2022-08-16 15:58:27 +0430

    edit user model

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\User\Role;
+use Laravel\Sanctum\HasApiTokens;
 use App\Models\Ticket\TicketAdmin;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -64,5 +65,10 @@ class User extends Authenticatable
     public function ticketAdmin()
     {
         return $this->hasOne(TicketAdmin::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
     }
 }
