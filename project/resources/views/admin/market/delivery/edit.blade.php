8264447d4e756149ebef0f3bd03d9cb463e4cb3c
amir <amirqaderi381@gmail.com>
2022-08-17 16:53:10 +0430

    complete delivery views

--- project/resources/views/admin/market/delivery/edit.blade.php
+++ project/resources/views/admin/market/delivery/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-    <title>ایجاد روش ارسال</title>
+    <title>ویرایش روش ارسال</title>
 @endsection
 
 @section('content')
@@ -10,7 +10,7 @@
             <li class="breadcrumb-item font-size-12"><a href="#">خانه</a></li>
             <li class="breadcrumb-item font-size-12"><a href="#"> بخش فروش</a></li>
             <li class="breadcrumb-item font-size-12"><a href="#"> روش های ارسال</a> </li>
-            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد روش ارسال</li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ویرایش روش ارسال</li>
         </ol>
     </nav>
 
@@ -18,7 +18,7 @@
         <section class="col-12">
             <section class="main-body-container">
                 <section class="main-body-container-header">
-                    <h5>ایجاد روش ارسال</h5>
+                    <h5>ویرایش روش ارسال</h5>
                 </section>
 
                 <section class="mt-4 mb-3 pb-2 border-bottom">
@@ -26,14 +26,14 @@
                 </section>
 
                 <section>
-                    <form action="{{ route('admin.market.delivery.store') }}" method="post">
+                    <form action="{{ route('admin.market.delivery.update',$delivery->id) }}" method="post">
                         @csrf
+                        @method('put')
                         <div class="form-row">
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="name">نام روش ارسال</label>
-                                    <input type="text" name="name" class="form-control form-control-sm"
-                                        value="{{ old('name') }}">
+                                    <input type="text" name="name" class="form-control form-control-sm" value="{{ old('name',$delivery->name) }}">
                                 </div>
                                 @error('name')
                                     <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
@@ -46,8 +46,7 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="amount">هزینه ارسال</label>
-                                    <input type="text" class="form-control form-control-sm" name="amount"
-                                        value="{{ old('amount') }}">
+                                    <input type="text" class="form-control form-control-sm" name="amount" value="{{ old('amount',$delivery->amount) }}">
                                 </div>
                                 @error('amount')
                                     <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
@@ -60,8 +59,7 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="delivery_time">زمان ارسال</label>
-                                    <input type="text" name="delivery_time" class="form-control form-control-sm"
-                                        value="{{ old('delivery_time') }}">
+                                    <input type="text" name="delivery_time" class="form-control form-control-sm" value="{{ old('delivery_time',$delivery->delivery_time) }}">
                                 </div>
                                 @error('delivery_time')
                                     <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
@@ -75,7 +73,7 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="delivery_time_unit">واحد زمان ارسال</label>
-                                    <input type="text" name="delivery_time_unit" class="form-control form-control-sm" value="delivery_time_unit">
+                                    <input type="text" name="delivery_time_unit" class="form-control form-control-sm" value="{{ old('delivery_time_unit',$delivery->delivery_time_unit) }}">
                                 </div>
                                 @error('delivery_time_unit')
                                     <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
@@ -90,8 +88,8 @@
                                 <div class="form-group">
                                     <label for="status">وضعیت</label>
                                     <select id="status" class="form-control form-control-sm" name="status">
-                                        <option value="0" {{ old('status') == 0 ? 'selected' : '' }}>غیر فعال</option>
-                                        <option value="1" {{ old('status') == 1 ? 'selected' : '' }}>فعال</option>
+                                        <option value="0" {{ old('status',$delivery->status) == 0 ? 'selected' : '' }}>غیر فعال</option>
+                                        <option value="1" {{ old('status',$delivery->status) == 1 ? 'selected' : '' }}>فعال</option>
                                     </select>
                                 </div>
                             </div>
--- project/resources/views/admin/market/delivery/index.blade.php
+++ project/resources/views/admin/market/delivery/index.blade.php
@@ -36,56 +36,41 @@
                                 <th scope="col">نام روش ارسال</th>
                                 <th scope="col">هزینه ارسال</th>
                                 <th scope="col">زمان ارسال</th>
+                                <th scope="col">وضعیت</th>
                                 <th scope="col" class="max-width-16-rem text-center"><i class="fas fa-cogs"></i> تنظیمات
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
+                        @foreach($delivery_methods as $delivery)
                             <tr>
-                                <th>1</th>
-                                <td>پست پیشتاز</td>
-                                <td>26000 تومان </td>
-                                <td> حداکثر دو روز کاری </td>
+                                <th>{{ $loop->iteration }}</th>
+                                <td>{{ $delivery->name }}</td>
+                                <td>{{ $delivery->amount }} تومان </td>
+                                <td>{{ $delivery->delivery_time . ' ' .$delivery->delivery_time_unit }}</td>
+                                <td>
+                                    <label>
+                                        <input id="{{ $delivery->id }}"
+                                            onchange="changeStatus({{ $delivery->id }})"
+                                            data-url="{{ route('admin.market.delivery.status', $delivery->id) }}"
+                                            type="checkbox" @if ($delivery->status === 1) checked @endif>
+                                    </label>
+                                </td>
                                 <td class="width-16-rem text-left">
-                                    <button type="submit" class="btn btn-primary btn-sm">
+                                    <a href="{{ route('admin.market.delivery.edit',$delivery->id) }}" class="btn btn-primary btn-sm">
                                         <i class="fas fa-edit"></i> ویرایش
-                                    </button>
+                                    </a>
 
-                                    <button type="submit" class="btn btn-danger btn-sm">
-                                        <i class="fas fa-trash-alt"></i> حذف
-                                    </button>
+                                    <form action="{{ route('admin.market.delivery.destroy',$delivery->id) }}" method="post" class="d-inline">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger btn-sm delete">
+                                            <i class="fas fa-trash-alt"></i> حذف
+                                        </button>
+                                    </form>
                                 </td>
                             </tr>
-                            <tr>
-                                <th>2</th>
-                                <td>پست پیشتاز</td>
-                                <td>26000 تومان </td>
-                                <td> حداکثر دو روز کاری </td>
-                                <td class="width-16-rem text-left">
-                                    <button type="submit" class="btn btn-primary btn-sm">
-                                        <i class="fas fa-edit"></i> ویرایش
-                                    </button>
-
-                                    <button type="submit" class="btn btn-danger btn-sm">
-                                        <i class="fas fa-trash-alt"></i> حذف
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>3</th>
-                                <td>پست پیشتاز</td>
-                                <td>26000 تومان </td>
-                                <td> حداکثر دو روز کاری </td>
-                                <td class="width-16-rem text-left">
-                                    <button type="submit" class="btn btn-primary btn-sm">
-                                        <i class="fas fa-edit"></i> ویرایش
-                                    </button>
-
-                                    <button type="submit" class="btn btn-danger btn-sm">
-                                        <i class="fas fa-trash-alt"></i> حذف
-                                    </button>
-                                </td>
-                            </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </section>
@@ -93,3 +78,76 @@
         </section>
     </div>
 @endsection
+
+@section('script')
+
+    <script type="text/javascript">
+
+        function changeStatus(id) {
+            let element = $('#' + id);
+            let url = element.attr('data-url')
+            let elementValue = !element.prop('checked'); // return ture/false
+
+            $.ajax({
+                url: url,
+                type: 'GET',
+                success: function(response) {
+                    if (response.status) {
+                        if (response.checked) {
+                            element.prop('checked', true);
+                            successToast('روش ارسال با موفقیت فعال شد')
+
+                        } else {
+                            element.prop('checked', false);
+                            successToast('روش ارسال با موفقیت غیر فعال شد')
+                        }
+                    } else {
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی بوجود امده است');
+                    }
+                },
+                error: function() {
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد');
+                }
+            });
+
+            function successToast(message) {
+                let successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-success text-white">\n' +
+                    '<strong class="ml-auto">' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    ' </button>\n' +
+                    '</section>\n' +
+                    '</section>\n';
+
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                })
+            };
+
+            function errorToast(message) {
+                let errorToastTags = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-danger text-white">\n' +
+                    '<strong class="ml-auto">' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    ' </button>\n' +
+                    '</section>\n' +
+                    '</section>\n';
+
+                $('.toast-wrapper').append(errorToastTags);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                });
+            }
+
+        }
+    </script>
+
+    @include('admin.alerts.sweetalert.confirm-delete', ['className' => 'delete'])
+
+@endsection
+
