fc08717a5e75024e646ee99c2bfa425e75554742
amir <amirqaderi381@gmail.com>
2022-08-18 17:07:48 +0430

    complete product category controller

--- project/app/Http/Controllers/Admin/Market/CategoryController.php
+++ project/app/Http/Controllers/Admin/Market/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Admin\Market;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Market\ProductCategory;
+use App\Http\Requests\Admin\Market\ProductCategoryRequest;
+use App\Http\Services\Image\ImageService;
 
 class CategoryController extends Controller
 {
@@ -14,7 +17,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('admin.market.category.index');
+        $product_categories = ProductCategory::orderBy('created_at','desc')->simplePaginate(15);
+        return view('admin.market.category.index',compact('product_categories'));
     }
 
     /**
@@ -24,7 +28,8 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        return view('admin.market.category.create');
+        $categories = ProductCategory::all();
+        return view('admin.market.category.create',compact('categories'));
     }
 
     /**
@@ -33,9 +38,21 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductCategoryRequest $request , ImageService $imageService)
     {
-        //
+        $inputs = $request->all();
+        if($request->hasFile('image'))
+        {
+            $imageService->setExclusiveDirectory('images'.DIRECTORY_SEPARATOR.'product_category');
+            $result=$imageService->createIndexAndSave($request->file('image'));
+            if($result == false)
+            {
+                return redirect()->route('admin.market.category.index')->with('swal-error','آپلود تصویر با خطا مواجه شد');
+            }
+        }
+        $inputs['image']=$result;
+        ProductCategory::create($inputs);
+        return redirect()->route('admin.market.category.index')->with('swal-success','دسته بندی جدید با موفقیت ثبت شد');
     }
 
     /**
@@ -44,9 +61,9 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(ProductCategory $productCategory)
     {
-        //
+         return view('admin.market.category.show',compact('productCategory'));
     }
 
     /**
@@ -55,9 +72,10 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(ProductCategory $productCategory)
     {
-        //
+        $categories = ProductCategory::all()->except($productCategory->id);
+        return view('admin.market.category.edit',compact('productCategory','categories'));
     }
 
     /**
@@ -67,9 +85,35 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductCategoryRequest $request ,ProductCategory $productCategory , ImageService $imageService)
     {
-        //
+        $inputs = $request->all();
+        if($request->hasFile('image'))
+        {
+            if(!empty($productCategory->image))
+            {
+                $imageService->deleteDirectoryAndFiles($productCategory->image['directory']);
+            }
+
+            $imageService->setExclusiveDirectory('images'.DIRECTORY_SEPARATOR.'product_category');
+            $result=$imageService->createIndexAndSave($request->file('image'));
+            if($result == false)
+            {
+                return redirect()->route('admin.market.category.index')->with('swal-error','آپلود تصویر با خطا مواجه شد');
+            }
+
+            $inputs['image']=$result;
+
+        }else{
+            if(isset($inputs['currentImage']) && !empty($productCategory->image))
+            {
+                $image = $productCategory->image;
+                $image['currentImage'] = $inputs['currentImage'];
+                $inputs['image'] = $image;
+            }
+        }
+        $productCategory->update($inputs);
+        return redirect()->route('admin.market.category.index')->with('swal-success','دسته بندی شما با موفقیت ویرایش شد');
     }
 
     /**
@@ -78,8 +122,54 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(ProductCategory $productCategory)
     {
-        //
+        $productCategory->delete();
+        return redirect()->route('admin.market.category.index')->with('swal-success','دسته بندی شما با موفقیت حذف شد');
+
+    }
+
+    public function status(ProductCategory $productCategory)
+    {
+        $productCategory->status = $productCategory->status == 0 ?  1 : 0;
+        $result = $productCategory->save();
+
+        if($result)
+        {
+            if($productCategory->status == 0)
+            {
+               return response()->json(['status' => true , 'checked' => false]);
+
+            }else{
+
+                return response()->json(['status' => true , 'checked' => true]);
+            }
+
+        }else{
+
+            return response()->json(['status' => false]);
+        }
+    }
+
+    public function showInMenu(ProductCategory $productCategory)
+    {
+        $productCategory->show_in_menu = $productCategory->show_in_menu == 0 ?  1 : 0;
+        $result = $productCategory->save();
+
+        if($result)
+        {
+            if($productCategory->show_in_menu == 0)
+            {
+               return response()->json(['show_in_menu' => true , 'checked' => false]);
+
+            }else{
+
+                return response()->json(['show_in_menu' => true , 'checked' => true]);
+            }
+
+        }else{
+
+            return response()->json(['show_in_menu' => false]);
+        }
     }
 }
