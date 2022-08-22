b18b15bda8d85d3d867a96f5b0202c6679f18f2f
amir <amirqaderi381@gmail.com>
2022-08-22 11:44:54 +0430

    add product request

--- project/app/Http/Requests/Admin/Market/ProductRequest.php
+++ project/app/Http/Requests/Admin/Market/ProductRequest.php
@@ -41,6 +41,8 @@ class ProductRequest extends FormRequest
                 'category_id' => 'required|min:1|max:100000000|regex:/^[0-9]+$/u|exists:product_categories,id',
                 'brand_id' => 'required|min:1|max:100000000|regex:/^[0-9]+$/u|exists:brands,id',
                 'published_at' => 'required|numeric',
+                'meta_names.*' =>'required',
+                'meta_values.*'=>'required'
             ];
 
         }else{
@@ -60,6 +62,8 @@ class ProductRequest extends FormRequest
                 'category_id' => 'required|min:1|max:100000000|regex:/^[0-9]+$/u|exists:product_categories,id',
                 'brand_id' => 'required|min:1|max:100000000|regex:/^[0-9]+$/u|exists:brands,id',
                 'published_at' => 'required|numeric',
+                'meta_names.*' =>'required',
+                'meta_values.*'=>'required'
             ];
         }
 
