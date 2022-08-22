baea2d2f14b7989a32304443e8316a4fcf6df985
amir <amirqaderi381@gmail.com>
2022-08-22 11:44:28 +0430

    add product color model

---
+++ project/app/Models/Market/ProductColor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Market;
+
+use App\Models\Market\Product;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ProductColor extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable=['color_name','product_id','price_increase','status','sold_number','frozen_number','marketable_number'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
