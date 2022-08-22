202b662c101637408c2ba32ce73c85a548d3ec01
amir <amirqaderi381@gmail.com>
2022-08-22 17:40:37 +0430

    add category attribute model

---
+++ project/app/Models/Market/CategoryAttribute.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Market;
+
+use App\Models\Market\ProductCategory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CategoryAttribute extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable=['name','type','unit','category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
+}
