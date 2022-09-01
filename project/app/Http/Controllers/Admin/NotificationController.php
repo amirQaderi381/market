3dc5eb37a2759b86d516eb4e7e1b9700214a9ed5
amir <amirqaderi381@gmail.com>
2022-09-01 11:34:39 +0430

    edit notification

--- project/app/Http/Controllers/Admin/NotificationController.php
+++ project/app/Http/Controllers/Admin/NotificationController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Notification;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class NotificationController extends Controller
