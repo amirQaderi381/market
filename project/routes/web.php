3ff16b51f1af37933c343fb94b196320bb32bfd4
amir <amirqaderi381@gmail.com>
2022-09-06 14:08:24 +0430

    add rate limiter for authenticate

--- project/routes/web.php
+++ project/routes/web.php
@@ -470,10 +470,10 @@ Route::namespace('Auth')->group(function(){
     Route::namespace('Customer')->group(function(){
 
         Route::get('/login-register',[LoginRegisterController::class , 'loginRegisterForm'])->name('auth.customer.login-register-form');
-        Route::post('/login-register',[LoginRegisterController::class , 'loginRegister'])->name('auth.customer.login-register');
+        Route::middleware('throttle:customer-auth-login-register-limiter')->post('/login-register',[LoginRegisterController::class , 'loginRegister'])->name('auth.customer.login-register');
         Route::get('/login-confirm/{token}',[LoginRegisterController::class , 'loginConfirmForm'])->name('auth.customer.login-confirm-form');
-        Route::post('/login-confirm/{token}',[LoginRegisterController::class , 'loginConfirm'])->name('auth.customer.login-confirm');
-        Route::get('/login-resend-otp/{token}',[LoginRegisterController::class , 'loginResendOtp'])->name('auth.customer.login-resend-otp');
+        Route::middleware('throttle:customer-auth-login-confirm-limiter')->post('/login-confirm/{token}',[LoginRegisterController::class , 'loginConfirm'])->name('auth.customer.login-confirm');
+        Route::middleware('throttle:customer-auth-login-resend-otp-limiter')->get('/login-resend-otp/{token}',[LoginRegisterController::class , 'loginResendOtp'])->name('auth.customer.login-resend-otp');
     });
 });
 
