a9520d03a7c00727f56e514c8c20667414a4188c
amir <amirqaderi381@gmail.com>
2022-09-08 12:35:42 +0430

    add home view

--- project/resources/views/customer/home.blade.php
+++ project/resources/views/customer/home.blade.php
@@ -3,23 +3,29 @@
 
 @section('content')
 
-
     <!-- start slideshow -->
     <section class="container-xxl my-4">
         <section class="row">
             <section class="col-md-8 pe-md-1 ">
                 <section id="slideshow" class="owl-carousel owl-theme">
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/1.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/2.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/3.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/4.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/5.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/6.gif" alt=""></a></section>
+                    @foreach($slideShows as $slideShow)
+                        <section class="item">
+                            <a class="w-100 d-block h-auto text-decoration-none" href="{{ urldecode($slideShow->url) }}">
+                              <img class="w-100 rounded-2 d-block h-auto" src="{{ asset($slideShow->image) }}" alt="">
+                            </a>
+                        </section>
+                    @endforeach
+
                 </section>
             </section>
             <section class="col-md-4 ps-md-1 mt-2 mt-md-0">
-                <section class="mb-2"><a href="#" class="d-block"><img class="w-100 rounded-2" src="assets/images/slideshow/12.gif" alt=""></a></section>
-                <section class="mb-2"><a href="#" class="d-block"><img class="w-100 rounded-2" src="assets/images/slideshow/11.jpg" alt=""></a></section>
+                @foreach($topSlideShows as $topSlideShow)
+                   <section class="mb-2">
+                        <a href="#" class="d-block">
+                            <img class="w-100 rounded-2" src="{{ asset($topSlideShow->image) }}" alt="">
+                        </a>
+                    </section>
+                @endforeach
             </section>
         </section>
     </section>
