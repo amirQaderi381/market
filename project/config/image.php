0d2daa419b9f172cdb8457a827e1a62c5f132c6c
amir <amirqaderi381@gmail.com>
2022-09-09 13:15:34 +0430

    edit config/image

--- project/config/image.php
+++ project/config/image.php
@@ -29,8 +29,8 @@ return [
 
         'medium' => [
 
-            'width' => 400,
-            'height' =>300
+            'width' => 350,
+            'height' =>350
         ],
 
         'small' => [
