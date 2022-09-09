b043e7fe4cc4421e520a618c7ce7126a0433be0c
amir <amirqaderi381@gmail.com>
2022-09-09 12:04:20 +0430

    edit home

--- project/app/Http/Controllers/Customer/HomeController.php
+++ project/app/Http/Controllers/Customer/HomeController.php
@@ -14,15 +14,15 @@ class HomeController extends Controller
     public function home()
     {
         $slideShows = Banner::where('position',0)->where('status',1)->get();
-        $topSlideShows = Banner::where('position',1)->where('status',1)->take(2)->get();
-        $middleSlideShows = Banner::where('position',2)->where('status',1)->take(2)->get();
-        $bottomSlideShow = Banner::where('position',3)->where('status',1)->first();
+        $topBanners = Banner::where('position',1)->where('status',1)->take(2)->get();
+        $middleBanners = Banner::where('position',2)->where('status',1)->take(2)->get();
+        $bottomBanner = Banner::where('position',3)->where('status',1)->first();
 
         $brands = Brand::all();
 
         $mostVisitedProducts = Product::latest()->take(10)->get();
         $offerProducts = Product::latest()->take(10)->get();
 
-        return view('customer.home',compact('slideShows','topSlideShows','middleSlideShows','bottomSlideShow','brands','mostVisitedProducts','offerProducts'));
+        return view('customer.home',compact('slideShows','topBanners','middleBanners','bottomBanner','brands','mostVisitedProducts','offerProducts'));
     }
 }
