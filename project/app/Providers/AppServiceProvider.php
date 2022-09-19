48757d8308f49ec42306d2750d449e340543882a
amir <amirqaderi381@gmail.com>
2022-09-19 13:12:19 +0430

    edit app service provider

--- project/app/Providers/AppServiceProvider.php
+++ project/app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
-use App\Models\Market\Comment;
 use App\Models\Notification;
+use App\Models\Market\Comment;
+use App\Models\Market\CartItem;
 use Illuminate\Support\Composer;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +31,14 @@ class AppServiceProvider extends ServiceProvider
        view()->composer('admin.layouts.header', function ($view) {
            $view->with('unseenComments',Comment::where('seen',0)->get());
            $view->with('notifications', Notification::where('read_at', null)->get());
+       });
+
+       view()->composer('customer.layouts.header',function($view)
+       {
+           if(Auth::check())
+           {
+              $view->with('cartItems',CartItem::where('user_id',Auth::user()->id)->get());
+           }
        });
     }
 }
