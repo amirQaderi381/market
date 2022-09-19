6d2fa15017b708fdf0a3fcbb152b9df636cc90cf
amir <amirqaderi381@gmail.com>
2022-09-19 13:13:08 +0430

    edit master

--- project/resources/views/customer/layouts/master-two-col.blade.php
+++ project/resources/views/customer/layouts/master-two-col.blade.php
@@ -18,6 +18,8 @@
     <main id="main-body-one-col" class="main-body">
 
        @yield('content')
+       <section class="toast-wrapper position-fixed p-4 flex-row-reverse" style="z-index: 909999999;right: 0; top: 3rem; width: 26rem; max-width: 80%;">
+       </section>
 
     </main>
 
@@ -26,11 +28,12 @@
 
 
     @include('customer.layouts.script')
+
     @yield('script')
 
-    <section class="toast-wrapper flex-row-reverse">
+    {{-- <section class="toast-wrapper flex-row-reverse">
         @include('customer.alerts.toast.success')
-    </section>
+    </section> --}}
 
     @include('customer.alerts.sweetalert.success')
     @include('customer.alerts.sweetalert.error')
