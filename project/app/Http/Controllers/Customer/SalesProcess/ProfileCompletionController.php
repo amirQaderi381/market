d29cd87e4932108933edb4fb5207345d1e03a1ab
amir <amirqaderi381@gmail.com>
2022-09-19 13:13:59 +0430

    add profile completion controller

---
+++ project/app/Http/Controllers/Customer/SalesProcess/ProfileCompletionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Customer\SalesProcess;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileCompletionController extends Controller
+{
+
+    public function profileCompletion()
+    {
+        dd('hi');
+    }
+
+    public function update()
+    {
+
+    }
+}
