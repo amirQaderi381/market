67339679f6840d2625dd5ee6491aed2e7a9174df
amir <amirqaderi381@gmail.com>
2022-09-21 15:30:56 +0430

    edit address controller

--- project/app/Http/Controllers/Customer/SalesProcess/AddressController.php
+++ project/app/Http/Controllers/Customer/SalesProcess/AddressController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Customer\SalesProcess;
 
+use App\Models\Province;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Models\Market\CartItem;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Customer\SalesProcess\StoreAddressRequest;
+use App\Models\Address;
 use Illuminate\Support\Facades\Auth;
 
 class AddressController extends Controller
@@ -12,17 +15,40 @@ class AddressController extends Controller
 
     public function addressAndDelivery()
     {
+        $user = Auth::user();
+        $cartItems = CartItem::where('user_id',$user->id)->get();
+        $provinces = Province::all();
 
-        if(empty(CartItem::where('user_id',Auth::user()->id)->count()))
+        if(empty(CartItem::where('user_id',$user->id)->count()))
         {
             return redirect()->route('customer.salesProcess.cart');
         }
 
 
+        return view('customer.sales-process.addressAndDelivery',compact('cartItems','provinces'));
+
     }
 
-    public function address()
-    {
 
+    public function getCities(Province $province)
+    {
+        $cities = $province->cities()->get();
+
+        if($cities !== null)
+        {
+            return response()->json(['status'=>true , 'cities'=>$cities]);
+
+        }else{
+
+            return response()->json(['status'=>false , 'cities'=>null]);
+        }
+    }
+
+    public function address(StoreAddressRequest $request)
+    {
+        $inputs = $request->all();
+        $inputs['user_id'] = Auth::user()->id;
+        Address::create($inputs);
+        return back()->with('swal-success','آدرس جدید شما با موفقیت ثبت شد');
     }
 }
