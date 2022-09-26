2325d82d252e8a724f06036bb907b06078c1bdad
amir <amirqaderi381@gmail.com>
2022-09-26 13:42:51 +0330

    edit payment controller

--- project/app/Http/Controllers/Customer/SalesProcess/PaymentController.php
+++ project/app/Http/Controllers/Customer/SalesProcess/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Customer\SalesProcess;
 use App\Models\Market\Copan;
 use App\Models\Market\Order;
 use Illuminate\Http\Request;
+use App\Models\Market\CartItem;
 use App\Http\Controllers\Controller;
 
 
@@ -12,7 +13,10 @@ class PaymentController extends Controller
 {
     public function payment()
     {
-        return view('customer.sales-process.payment');
+        $user= auth()->user();
+        $cartItems = CartItem::where('user_id',$user->id)->get();
+        $order = Order::where(['user_id'=> $user->id , 'order_status'=>0])->first();
+        return view('customer.sales-process.payment',compact('cartItems','order'));
     }
 
     public function copanDiscount(Request $request)
@@ -22,7 +26,7 @@ class PaymentController extends Controller
 
         $copan = Copan::where([ ['code',$request->copan] , ['status',1] , ['start_date','<',now()] ,['end_date','>',now()] ])->first();
 
-        if($copan)
+        if($copan != null)
         {
 
             if($copan->user_id != null)
@@ -31,12 +35,11 @@ class PaymentController extends Controller
 
                 if($copan == null)
                 {
-                    return back()->with('swal-error','کد تخفیف وارد شده معتبر نیست');
+                    return back()->with('swal-error','کد تخفیف وارد شده معتبر نی باشد');
                 }
-
             }
 
-            $order = Order::where(['user_id',auth()->user()->id] , ['order_status',0] , ['copan_id',null])->first();
+            $order = Order::where(['user_id'=> auth()->user()->id , 'order_status'=>0 , 'copan_id'=>null])->first();
 
             if($order)
             {
@@ -44,10 +47,12 @@ class PaymentController extends Controller
                 {
                     $copanDiscountAmount = $order->order_final_amount * ($copan->amount / 100);
 
-                    if($copanDiscountAmount > $order->discount_ceiling)
+
+                    if($copanDiscountAmount > $copan->discount_ceiling)
                     {
-                        $copanDiscountAmount = $order->discount_ceiling;
+                        $copanDiscountAmount = $copan->discount_ceiling;
                     }
+
 
                 }else
                 {
@@ -58,9 +63,43 @@ class PaymentController extends Controller
                 $finalDiscount = $order->order_total_products_discount_amount + $copanDiscountAmount;
 
                 $order->update(['copan_id'=>$copan->id , 'order_copan_discount_amount'=>$copanDiscountAmount , 'order_total_products_discount_amount'=>$finalDiscount , 'order_final_amount'=>$order_final_amount]);
+
+                return back()->with('toast-success','کد تخفیف با موفقیت اعمال شد');
+
+            }else{
+
+                return back()->with('swal-error','کد تخفیف وارد شده قبلا استفاده شده است');
+
             }
 
+        }else{
+
+            return back()->with('swal-error','کد تخفیف وارد شده معتبر نمی باشد');
         }
 
+    }
+
+    public function paymentSubmit(Request $request)
+    {
+        $validate = $request->validate(['payment_type'=>'required']);
+
+        switch ($request->payment_type) {
+
+            case 1:
+                dd('online');
+                break;
+
+            case 2:
+                dd('offline');
+                break;
+
+            case 3:
+                dd('cash');
+                break;
+
+            default:
+                return back();
+                break;
+        }
     }
 }
